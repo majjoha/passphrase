8adfc1a173b7492966295760762901a756a9e4ef
Mathias Jean Johansen <mathias@mjj.io>
2020-12-11 14:25:18 +0100

    Add function for generating a sequence of random `Int`s.

--- src/Passphrase/Dice.hs
+++ src/Passphrase/Dice.hs
@@ -1,6 +1,13 @@
 module Passphrase.Dice
   ( joinDigits
+  , rollDice
   ) where
+
+import           Control.Monad (replicateM)
+import           System.Random
 
 joinDigits :: [Int] -> Int
 joinDigits = foldl ((+) . (* 10)) 0
+
+rollDice :: Int -> Int -> Int -> IO [Int]
+rollDice n start end = replicateM n $ randomRIO (start, end)
--- test/Passphrase/DiceSpec.hs
+++ test/Passphrase/DiceSpec.hs
@@ -10,3 +10,7 @@ spec = do
       joinDigits [] `shouldBe` 0
       joinDigits [1] `shouldBe` 1
       joinDigits [1, 2, 3] `shouldBe` 123
+
+  describe "rollDice" $ do
+    it "returns a sequence of random numbers in a range" $ do
+      rollDice 2 1 1 `shouldReturn` [1, 1]
