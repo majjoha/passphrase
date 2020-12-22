75df0643937ff6f0cdb690ecd1f8986910653791
Mathias Jean Johansen <mathias@mjj.io>
2020-12-22 10:08:32 +0100

    Add `passphrase` function.

--- src/Passphrase/Passphrase.hs
+++ src/Passphrase/Passphrase.hs
@@ -1,4 +1,25 @@
-module Passphrase.Passphrase where
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Passphrase.Passphrase
+  ( passphrase
+  , toTuple
+  ) where
 
-passphrase :: IO ()
-passphrase = putStrLn "passphrase"
+import           Passphrase.Dice (joinDigits)
+import           RIO
+import qualified RIO.List        as L
+import qualified RIO.Map         as Map
+
+passphrase :: String -> [[Integer]] -> String
+passphrase wordlist dice =
+  unwords $ map (\index -> Map.findWithDefault "" index wordMap) indices
+    where
+      indices = map joinDigits dice
+      wordMap = Map.fromList $ map (toTuple . words) $ lines wordlist
+
+toTuple :: [String] -> (Integer, String)
+toTuple [] = (0, "")
+toTuple (index:word) = (fromMaybe 0 index', fromMaybe "" word')
+  where
+    index' = readMaybe index :: Maybe Integer
+    word' = L.headMaybe word
