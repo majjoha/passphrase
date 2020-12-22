eecfa785927bca54acf1709e0eebab158c9fc4ea
Mathias Jean Johansen <mathias@mjj.io>
2020-12-22 10:10:39 +0100

    Add entry point for generating passphrases.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Passphrase.Passphrase (passphrase)
+import           Control.Monad (replicateM)
+import           Passphrase    (passphrase, rollDice)
+import           RIO
+import qualified RIO.Text      as T
 
 main :: IO ()
-main = passphrase
+main = do
+  wordlist <- T.unpack <$> readFileUtf8 "data/eff-large-wordlist.txt"
+  dice <- replicateM 6 $ rollDice 5 1 6
+  runSimpleApp $ do
+    logInfo . display . T.pack $ passphrase wordlist dice
