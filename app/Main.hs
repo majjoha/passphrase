36207999888d8b3541cdaa6ea94940f0c94f87b4
Simon Shine <shreddedglory@gmail.com>
2021-01-29 11:42:57 +0100

    Allow custom wordlists to be used (fixes #1)
    This provides a basic optparse-applicative argument parser:
    
    ```
    $ passphrase --help
    Passphrase
    
    Usage: passphrase [WORDFILE]
        Diceware passphrase generator
    
    Available options:
        -h,--help                Show this help text
        WORDFILE                 Word list file
    ```

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,42 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
 import           Control.Monad (replicateM)
+import           Options.Applicative
 import           Passphrase    (passphrase, rollDice)
 import           RIO
 import qualified RIO.Text      as T
 
+data Arguments = Arguments
+  { wordlistFilePath :: FilePath
+  }
+
+defaultWordlist :: FilePath
+defaultWordlist = "data/eff-large-wordlist.txt"
+
 main :: IO ()
 main = do
-  wordlist <- T.unpack <$> readFileUtf8 "data/eff-large-wordlist.txt"
+  Arguments {..} <- execParser argumentsInfo
+  wordlist <- T.unpack <$> readFileUtf8 wordlistFilePath
   dice <- replicateM 6 $ rollDice 5 1 6
   runSimpleApp $ do
     logInfo . display . T.pack $ passphrase wordlist dice
+
+argumentsInfo :: ParserInfo Arguments
+argumentsInfo =
+  info (helper <*> arguments) $
+       fullDesc
+    <> header "Passphrase"
+    <> progDesc "Diceware passphrase generator"
+
+arguments :: Parser Arguments
+arguments = Arguments
+  <$> wordlistFilePathArgument
+  where
+    wordlistFilePathArgument =
+      strArgument $
+           help "Word list file"
+        <> value defaultWordlist
+        <> metavar "WORDFILE"
